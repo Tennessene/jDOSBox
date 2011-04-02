b1868fd489b6720cb6af21e5b637596eb07a297d
danoon2 <jwbryant72@yahoo.com>
2011-04-02 22:51:24 +0000

    Bumped version to 19
    git-svn-id: https://svn.code.sf.net/p/jdosbox/code/trunk@318 4cb9e868-4783-4b93-94d1-9e8e689819c6

--- src/jdos/misc/setup/Config.java
+++ src/jdos/misc/setup/Config.java
@@ -15,7 +15,7 @@ import java.util.Iterator;
 
 public class Config {
     static public final String MAJOR_VERSION = "0.74";
-    static public final String VERSION = "0.74.18";
+    static public final String VERSION = "0.74.19";
     static public final boolean C_DYNAMIC_X86 = false;
     static public final boolean C_DYNREC = false;
     static public final boolean C_FPU = true;

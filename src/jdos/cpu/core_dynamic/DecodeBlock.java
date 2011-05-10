42360f823110de4ca2982e9c9551826ede9b7d54
danoon2 <jwbryant72@yahoo.com>
2011-05-10 04:12:26 +0000

    Caching core fix for illegal instructions
    git-svn-id: https://svn.code.sf.net/p/jdosbox/code/trunk@344 4cb9e868-4783-4b93-94d1-9e8e689819c6

--- src/jdos/cpu/core_dynamic/DecodeBlock.java
+++ src/jdos/cpu/core_dynamic/DecodeBlock.java
@@ -33,6 +33,9 @@ final public class DecodeBlock {
                     if (o.c>=0) Debug.stop(Debug.TYPE_CPU, o.c);
                 o = o.next;
             }
+            if (result == Constants.BR_Illegal) {
+                CPU_Regs.reg_eip=o.eip;
+            }
         } catch (SMC_Exception e) {
             System.out.println("SMC");
             CPU_Regs.reg_eip(o.eip);

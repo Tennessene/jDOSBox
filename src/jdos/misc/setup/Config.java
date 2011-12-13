6d9406b7c0fced5c964ca888c27d01a4f38680df
danoon2 <jwbryant72@yahoo.com>
2011-12-13 23:46:55 +0000

    git-svn-id: https://svn.code.sf.net/p/jdosbox/code/trunk@507 4cb9e868-4783-4b93-94d1-9e8e689819c6

--- src/jdos/misc/setup/Config.java
+++ src/jdos/misc/setup/Config.java
@@ -11,7 +11,7 @@ import java.util.Vector;
 
 public class Config {
     static public final String MAJOR_VERSION = "0.74";
-    static public final String VERSION = "0.74.25b1";
+    static public final String VERSION = "0.74..25c_svn";
     static public final boolean C_DYNAMIC = true;
     static public final boolean C_DYNREC = false;
     static public final boolean C_FPU = true;

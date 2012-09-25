72d9ff16f330f2c611519e27079e3fc85b243d08
danoon2 <jwbryant72@yahoo.com>
2012-09-25 23:29:27 +0000

    Dosbox svn 3681 keep old key-flags for buggy layout files that redefine/extend already defined keys (fixes caps-lock behaviour of key E for us-layout with codepage 858)
    git-svn-id: https://svn.code.sf.net/p/jdosbox/code/trunk@572 4cb9e868-4783-4b93-94d1-9e8e689819c6

--- src/jdos/dos/Dos_keyboard_layout.java
+++ src/jdos/dos/Dos_keyboard_layout.java
@@ -414,7 +414,14 @@ public class Dos_keyboard_layout {
 //                        }
 //                    }
 //
-//                    current_layout[scan*layout_pages+layout_pages-1]=read_buf[read_buf_pos-2];	// flags
+//                    // calculate max length of entries, taking into account old number of entries
+//				      Bit8u new_flags=current_layout[scan*layout_pages+layout_pages-1]&0x7;
+//				      if ((read_buf[read_buf_pos-2]&0x7) > new_flags) new_flags = read_buf[read_buf_pos-2]&0x7;
+//
+//				      // merge flag bits in as well
+//				      new_flags |= (read_buf[read_buf_pos-2] | current_layout[scan*layout_pages+layout_pages-1]) & 0xf0;
+//
+//				      current_layout[scan*layout_pages+layout_pages-1]=new_flags;
 //                    if (read_buf[read_buf_pos-2]&0x80) scan_length*=2;		// granularity flag (S)
 //                }
 //                i+=scan_length;		// advance pointer
